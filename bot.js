66bd0d0f3fc56b958f6efe4d8d0d9c9bf35807c7
Unknown <Alee14498@gmail.com>
2017-10-01 20:15:37 -0400

    New commands and updated commands and 1 bug

--- bot.js
+++ bot.js
@@ -1,4 +1,10 @@
-const Discord = require('discord.js');
+/*
+Reminder for tyson to create a new command do 
+if (command === 'CommandNameHere'){
+	
+}
+
+*/
 const client = new Discord.Client();
 const config = require('./config.json');
    
@@ -45,7 +51,7 @@ client.on('ready', () => {
 client.on("message", function(message){
     if (message.author.bot) return;
     if (message.channel.type === "dm") return;
-    if (message.content.indexOf(config.prefix) !== 0) return;
+    if (message.content.indexOf(prefix) !== 0) return;
 	let command = message.content.split(" ")[0];
 	command = command.slice(prefix.length);
 	let args = message.content.split(" ").slice(1);
@@ -75,8 +81,8 @@ client.on("message", function(message){
 		 var embed = new Discord.RichEmbed()
 		.setAuthor("Information", "https://images-ext-2.discordapp.net/external/KLnOX8cIR8Fe6aRozJDuwtMC4NQttUMJu3MQRy2nTvM/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/354594001736957954/b7259f4a3009c964cae30157176bc2ac.png?width=250&height=250")
         .setDescription("This information command will tell you all the information you need about T-BOT")
-        .addField("Bot Creators/Helpers", "Alee14-Making TBot\nTyson-Making and developng the Bot")
-        .addField("Why was this bot made and when was this bot made", "This bot was made on the 21th of September 2017 the bot was made because i have always wanted to code a great standing public bot and becuase i love coding discord bots and thank Alee for remaking it from AleeBot\'s 0.0.6 code base")
+        .addField("Bot Creators/Helpers", "Alee14-Converting AleeBot 0.0.6's code to TBot\nTyson-Making and developng the Bot")
+        .addField("Why was this bot made and when was this bot made", "This bot was made on the 21th of September 2017 the bot was made because i have always wanted to code a great standing public bot and becuase i love coding discord bots and thank Alee for remaking it from AleeBot\'s 0.0.6 code base and he gave me permission to use it.")
 		message.channel.sendEmbed(embed);
 	}
 	
@@ -84,6 +90,15 @@ client.on("message", function(message){
 		message.reply('Want to help contribute TBot here\'s the link: https://github.com/fun-with-tyson/TBot')
 	}
 	
+	if(command === 'say'){
+		if(message.author.id !== config.ownerID)   
+		message.reply(":no_entry: **NOPE!** Sorry buddy Tyson is only allow to say this command.") 
+		else {
+		message.channel.sendMessage(args.join(" "));
+		message.delete();
+		}
+	}
+});
 
  });
 
